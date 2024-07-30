c5cd98096aa60e58736d05bd512de9d8587248e3
ilhamfakihhilmi <ilhamfakih50@gmail.com>
2024-07-30 11:39:37 +0700

    first commit

---
+++ src/HelloVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <h4>Project vue pertama saya</h4>
+    <h4>nama saya: {{ nama }}</h4>
+    <h4>umur saya: {{ umur }} tahun</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nama: "fakih hilmi",
+      umur: 25,
+    };
+  },    
+  mounted() {
+    this.umur = this.umur + 3;
+  },
+};
+</script>
+
+<style></style>
