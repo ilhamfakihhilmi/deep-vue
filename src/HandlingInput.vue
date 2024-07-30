661610048a16c7740b1d4ce83a9ca890c98bbca5
ilhamfakihhilmi <ilhamfakih50@gmail.com>
2024-07-30 11:57:04 +0700

    handling user input

---
+++ src/HandlingInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Deret Bilangan Ganjil</h1>
+    <h4>{{ bilangan }}</h4>
+    <button v-on:click="next" >Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bilangan: 1,
+    };
+    },
+  methods: {
+      next() {
+          this.bilangan += 6;
+    }
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './HelloVue.vue'
+import App from './HandlingInput.vue'
 
 createApp(App).mount('#app')
